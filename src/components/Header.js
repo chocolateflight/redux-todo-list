b3f3233d80e8a29a13d8c6f59b4dc5621a78d985
Marc <67051612+chocolateflight@users.noreply.github.com>
2022-11-05 18:58:09 +0100

    added ugly github link

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,6 +11,7 @@ const Header = () => {
   return (
     <header className='w-full h-[70px] bg-rose-300 flex items-center justify-between mb-5 p-2 md:px-10'>
       <h1 className='font-bold text-2xl'>Redux ToDo</h1>
+      <a className='text-sky-800 italic hover:text-sky-200' href="https://github.com/chocolateflight" target="_blank" rel="noopener noreferrer">Visit my GitHub</a>
       <button
         className='border p-2 italic rounded-md shadow-md hover:bg-rose-200'
         onClick={clearHandler}>
